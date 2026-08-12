e447c1da27ae76a27428fde4385a010d70db08e6
N9+ <pinchodev91@gmail.com>
2026-08-12 18:03:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
+// ============================================================
+// ACTUALIZACIÓN DEL README DESACTIVADA TEMPORALMENTE
+//
+// ninpl.com está fuera de servicio.
+// No consultar el RSS ni modificar README.md.
+//
+// Cuando el dominio vuelva a estar disponible, restaurar
+// la lógica original.
+// ============================================================
+
 // Importacion de lo necesario
-import { promises as fs } from 'fs'
-import fetch from 'node-fetch'
-import Parser from 'rss-parser'
-
-var today = new Date();
-var hoy = today.toLocaleDateString('es-ES');
-const parser = new Parser()
-const obtenerUltimosArticulos = () =>
-  parser.parseURL('https://ninpl.com/index.xml').then((data) => data.items)
-
+//import { promises as fs } from 'fs'
+//import fetch from 'node-fetch'
+//import Parser from 'rss-parser'
+//var today = new Date();
+//var hoy = today.toLocaleDateString('es-ES');
+//const parser = new Parser()
+//const obtenerUltimosArticulos = () =>
+//parser.parseURL('https://ninpl.com/index.xml').then((data) => data.items)
 // Crear la funcion
-;(async () => {
-  const [plantilla, articulos] = await Promise.all([
-    fs.readFile('./README.md.tpl', { encoding: 'utf-8' }),
-    obtenerUltimosArticulos()
-  ])
-
+//;(async () => {
+//const [plantilla, articulos] = await Promise.all([
+//fs.readFile('./README.md.tpl', { encoding: 'utf-8' }),
+//obtenerUltimosArticulos()
+//])
 // Crear markdown con los ultimos articulos
-const ultimosArticulosMD = articulos
-   .slice(0, 5)
-   .map(({ title, link }) => `- [${title}](${link})`)
-   .join('\n')
-
+//const ultimosArticulosMD = articulos
+//.slice(0, 5)
+//.map(({ title, link }) => `- [${title}](${link})`)
+//.join('\n')
 // Reemplazar con la informacion obtenida
-const nuevoMD = plantilla
-  .replace('%{{ultimos}}%', ultimosArticulosMD)
-  .replace('%{{fecha}}%', hoy)
+//const nuevoMD = plantilla
+//.replace('%{{ultimos}}%', ultimosArticulosMD)
+//.replace('%{{fecha}}%', hoy)
+//await fs.writeFile('README.md', nuevoMD)
+//})()
 
-await fs.writeFile('README.md', nuevoMD)
-})()
+console.log(
+    'Actualización del README desactivada temporalmente: ninpl.com está fuera de servicio.'
+)
